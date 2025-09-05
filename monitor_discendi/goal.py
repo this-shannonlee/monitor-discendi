2f58d28f9fa9b1c33953606a72f709dc33c0c813
Shannon Lee <120006348+this-shannonlee@users.noreply.github.com>
2025-09-05 00:05:29 -0400

    working on goal crud with html

--- monitor_discendi/goal.py
+++ monitor_discendi/goal.py
@@ -20,3 +20,92 @@ def index():
     db = get_db()
     goals = db.execute("SELECT * FROM goal ORDER BY focus_level ASC").fetchall()
     return render_template("goal/index.html", goals=goals)
+
+
+@bp.route("/create", methods=("GET", "POST"))
+def create():
+    if request.method == "POST":
+        title = request.form["title"]
+        synopsis = request.form["synopsis"]
+        url_address = request.form["url_address"]
+        focus_level = request.form["focus_level"]
+        due_date = request.form["due_date"]
+        notes = request.form["notes"]
+        is_complete = request.form.get("is_complete", 0, type=int)
+        error = None
+
+        if not title:
+            error = "Title is required."
+        elif not focus_level:
+            error = "Focus level is required."
+
+        if error is None:
+            db = get_db()
+            db.execute(
+                "INSERT INTO goal (title, synopsis, url_address, focus_level, due_date, notes, is_complete)"
+                " VALUES (?, ?, ?, ?, ?, ?, ?)",
+                (
+                    title,
+                    synopsis,
+                    url_address,
+                    focus_level,
+                    due_date,
+                    notes,
+                    is_complete,
+                ),
+            )
+            db.commit()
+            return redirect(url_for("goal.index"))
+
+        flash(error)
+
+    return render_template("goal/create.html")
+
+
+@bp.route("/<int:id>", methods=("GET",))
+def read(id):
+    db = get_db()
+    goal = db.execute("SELECT * FROM goal WHERE id = ?", (id,)).fetchone()
+    objectives = db.execute(
+        "SELECT * FROM objective WHERE goal_id = ?", (id,)
+    ).fetchall()
+    if goal is None:
+        flash("Goal not found.")
+        return redirect(url_for("goal.index"))
+
+    return render_template("goal/read.html", goal=goal, objectives=objectives)
+
+
+@bp.route("/<int:id>/edit", methods=("GET", "POST"))
+def update(id):
+    db = get_db()
+    goal = db.execute("SELECT * FROM goal WHERE id = ?", (id,)).fetchone()
+
+    if goal is None:
+        flash("Goal not found.")
+        return redirect(url_for("goal.index"))
+
+    if request.method == "POST":
+        title = request.form["title"]
+        description = request.form["description"]
+        focus_level = request.form["focus_level"]
+        error = None
+
+        if not title:
+            error = "Title is required."
+        elif not description:
+            error = "Description is required."
+        elif not focus_level:
+            error = "Focus level is required."
+
+        if error is None:
+            db.execute(
+                "UPDATE goal SET title = ?, description = ?, focus_level = ? WHERE id = ?",
+                (title, description, focus_level, id),
+            )
+            db.commit()
+            return redirect(url_for("goal.index"))
+
+        flash(error)
+
+    return render_template("goal/edit.html", goal=goal)
--- monitor_discendi/schema.sql
+++ monitor_discendi/schema.sql
@@ -1,63 +1,65 @@
-DROP TABLE IF EXISTS goal;
+DROP TABLE if EXISTS goal;
 
-DROP TABLE IF EXISTS objective;
+DROP TABLE if EXISTS objective;
 
-DROP TABLE IF EXISTS plan;
+DROP TABLE if EXISTS plan;
 
-DROP TABLE IF EXISTS assignment_progress;
+DROP TABLE if EXISTS assignment_progress;
 
-DROP TABLE IF EXISTS assignment_source;
+DROP TABLE if EXISTS assignment_source;
 
-DROP TABLE IF EXISTS assignment;
+DROP TABLE if EXISTS assignment;
 
-DROP TABLE IF EXISTS topic;
+DROP TABLE if EXISTS topic;
 
-DROP TABLE IF EXISTS assignment_topic;
+DROP TABLE if EXISTS assignment_topic;
 
 CREATE TABLE goal (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT UNIQUE NOT NULL,
-    synopsis TEXT,
-    url_address TEXT,
+    id INTEGER PRIMARY key autoincrement,
+    title text UNIQUE NOT NULL,
+    synopsis text,
+    url_address text,
     focus_level INTEGER NOT NULL CHECK (
         focus_level > 0
         AND focus_level <= 10
     ) DEFAULT 2,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created TIMESTAMP NOT NULL DEFAULT current_timestamp,
     due_date TIMESTAMP,
-    notes TEXT,
+    notes text,
     is_complete BOOLEAN CHECK (is_complete IN (0, 1)) DEFAULT 0
 );
 
 CREATE TABLE objective (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY key autoincrement,
     goal_id INTEGER NOT NULL,
-    title TEXT NOT NULL UNIQUE,
-    synopsis TEXT,
-    url_address TEXT,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title text NOT NULL UNIQUE,
+    synopsis text,
+    url_address text,
+    created TIMESTAMP NOT NULL DEFAULT current_timestamp,
     is_complete BOOLEAN CHECK (is_complete IN (0, 1)),
-    FOREIGN KEY (goal_id) REFERENCES goal (id)
+    FOREIGN key (goal_id) REFERENCES goal (id)
 );
 
 CREATE TABLE plan (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY key autoincrement,
     objective_id INTEGER NOT NULL,
-    title TEXT NOT NULL,
-    synopsis TEXT,
-    url_address TEXT,
-    notes TEXT,
+    title text NOT NULL,
+    synopsis text,
+    url_address text,
+    notes text,
     is_complete BOOLEAN CHECK (is_complete IN (0, 1)),
-    FOREIGN KEY (objective_id) REFERENCES objective (id)
+    FOREIGN key (objective_id) REFERENCES objective (id)
 );
 
 CREATE TABLE assignment_source (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    kind TEXT NOT NULL UNIQUE
+    id INTEGER PRIMARY key autoincrement,
+    kind text NOT NULL UNIQUE
 );
 
-INSERT INTO assignment_source (id, kind)
-VALUES (1, 'lesson'),
+INSERT INTO
+    assignment_source (id, kind)
+VALUES
+    (1, 'lesson'),
     (2, 'tutorial'),
     (3, 'article'),
     (4, 'video'),
@@ -66,64 +68,44 @@ VALUES (1, 'lesson'),
     (7, 'examination');
 
 CREATE TABLE assignment_progress (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    progress TEXT NOT NULL UNIQUE
+    id INTEGER PRIMARY key autoincrement,
+    progress text NOT NULL UNIQUE
 );
 
-INSERT INTO assignment_progress (id, progress)
-VALUES (1, 'not started'),
+INSERT INTO
+    assignment_progress (id, progress)
+VALUES
+    (1, 'not started'),
     (2, 'on track'),
     (3, 'behind'),
     (4, 'on hold'),
     (5, 'completed');
 
 CREATE TABLE assignment (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY key autoincrement,
     plan_id INTEGER NOT NULL,
-    title TEXT NOT NULL,
-    synopsis TEXT,
-    url_address TEXT,
-    source_id INTEGER NOT NULL DEFAULT 1,
-    progress_id INTEGER NOT NULL DEFAULT 1,
-    notes TEXT,
+    title text NOT NULL,
+    synopsis text,
+    url_address text,
+    source_id INTEGER NOT NULL,
+    progress_id INTEGER NOT NULL,
+    notes text,
+    is_digested BOOLEAN CHECK (is_complete IN (0, 1)),
     is_complete BOOLEAN CHECK (is_complete IN (0, 1)),
-    FOREIGN KEY (plan_id) REFERENCES plan (id),
-    FOREIGN KEY (source_id) REFERENCES assignment_source (id),
-    FOREIGN KEY (progress_id) REFERENCES assignment_progress (id)
+    FOREIGN key (plan_id) REFERENCES plan (id),
+    FOREIGN key (source_id) REFERENCES assignment_source (id),
+    FOREIGN key (progress_id) REFERENCES assignment_progress (id)
 );
 
 CREATE TABLE topic (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL UNIQUE
+    id INTEGER PRIMARY key autoincrement,
+    title text NOT NULL UNIQUE
 );
 
 CREATE TABLE assignment_topic (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY key autoincrement,
     topic_id INTEGER NOT NULL,
     assignment_id INTEGER NOT NULL,
-    FOREIGN KEY (topic_id) REFERENCES topic (id),
-    FOREIGN KEY (assignment_id) REFERENCES assignment (id)
+    FOREIGN key (topic_id) REFERENCES topic (id),
+    FOREIGN key (assignment_id) REFERENCES assignment (id)
 );
-
-INSERT INTO goal (id, title, synopsis, url_address, focus_level)
-VALUES (
-        1,
-        'AWS Certified Cloud Practitioner',
-        'The AWS Certified Cloud Practitioner validates foundational, high-level understanding of AWS Cloud, services, and terminology.',
-        'https://aws.amazon.com/certification/certified-cloud-practitioner/',
-        1
-    ),
-    (
-        2,
-        'Microsoft Certified: Azure Administrator Associate',
-        'Demonstrate key skills to configure, manage, secure, and administer key professional functions in Microsoft Azure.',
-        'https://learn.microsoft.com/en-us/certifications/azure-administrator/',
-        2
-    ),
-    (
-        3,
-        'Microsoft Certified: Azure Solutions Architect Expert',
-        'Subject matter expertise in designing cloud and hybrid solutions that run on Azure',
-        'https://learn.microsoft.com/en-us/credentials/certifications/azure-solutions-architect/',
-        2
-    );
